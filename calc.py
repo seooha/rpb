2543467456a4c57c3da264f425cfe0fcc0b3e1c7
서하 <haeruing@naver.com>
2026-03-30 18:41:49 +0900

    Update calc.py

---
+++ calc.py
@@ -0,0 +1,22 @@
+def main():
+    print("Let's implement division. Type two numbers for x and y")
+    
+    x = int(input("x > "))
+    y = int(input("y > "))
+    
+    print("%d / %d = %0.3f" % (x, y, divide(x, y)))
+    
+    
+def add(a, b):
+    return a + b
+    
+def divide(a, b):
+    if b == 0:
+        print("Error: cannot divide by zero.")
+        exit()
+    else:
+        return round(a / b, 3)
+
+
+if __name__ == "__main__":
+    main()
